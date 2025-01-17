d3dc7f66664cc71696fd082ac4125c77c8a2f0c2
Roaring Robotics 14436 <57073397+roaringrobotics@users.noreply.github.com>
2025-01-17 11:10:46 -0700

    RotateCCW can now got CW, drive straight x is also added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PathingMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PathingMethods.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class PathingMethods {
     Hardware hw;
 
-    public double correction(double headingDelta) {
+    public static double correction(double headingDelta) {
         double correction = 0.0;
         if (Math.abs(headingDelta) > 0.02) {
             correction = 0.1;
@@ -78,6 +78,69 @@ public class PathingMethods {
             }
         }
     }
+    public static void driveStraightX(Hardware hw, LinearOpMode op, double distanceInches, double power)
+    {
+
+        double xloc = Math.abs(hw.pinpoint.getPosX() * hw.mmToInch);
+        double head = hw.pinpoint.getHeading();
+        double initx = Math.abs(hw.pinpoint.getPosY() * hw.mmToInch);
+        double initHeader = hw.pinpoint.getHeading();
+        double hdelta = head - initHeader;
+        double correction = 0;
+        double targetdelta = distanceInches - xloc;
+
+        double sign = 1;
+
+        if (distanceInches < 0) {
+            sign = -1;
+            power *= sign;
+            distanceInches *= sign;
+            initx *= sign;
+        }
+        double target = initx + distanceInches;
+        targetdelta = target - xloc;
+        while (targetdelta > 0 && !op.isStopRequested())
+        {
+            hw.pinpoint.update();
+            xloc = hw.pinpoint.getPosY() * hw.mmToInch * sign;
+            head = hw.pinpoint.getHeading();
+            targetdelta = target - xloc;
+            hdelta = head - initHeader;
+            double xDelta = xloc - initx;
+            power = PathingMethods.powerRamp(targetdelta, xDelta, power);
+            power *= sign;
+            op.telemetry.addData("bot heading)", head);
+            op.telemetry.addData("Position Y", xloc);
+            op.telemetry.addData("Initial y", initx);
+            op.telemetry.addData("power", power);
+            op.telemetry.addData("Delta y", targetdelta);
+            op.telemetry.addData("Delta Heading", hdelta);
+
+            correction = 0;
+            op.telemetry.update();
+            if (Math.abs(hdelta) > .02) {
+                correction = 0.1;
+            }
+
+            if (hdelta > 0) {
+                hw.frontLeft.setPower(-power + correction);
+                hw.frontRight.setPower(power - correction);
+                hw.backLeft.setPower(power + correction);
+                hw.backRight.setPower(-power - correction);
+            } else if (hdelta < 0) {
+                hw.frontLeft.setPower(-power - correction);
+                hw.frontRight.setPower(power + correction);
+                hw.backLeft.setPower(power - correction);
+                hw.backRight.setPower(-power + correction);
+            } else {
+                hw.frontLeft.setPower(-power);
+                hw.frontRight.setPower(power);
+                hw.backLeft.setPower(power);
+                hw.backRight.setPower(-power);
+            }
+        }
+    }
+
 
     public static double powerRamp(double delta, double traveled, double nominalPower) {
         nominalPower = Math.min(nominalPower, 0.5);
@@ -99,9 +162,13 @@ public class PathingMethods {
 
     }
 
-    public static double RotateCCW(Hardware hw, LinearOpMode lo, double targetDegrees, double power) {
-        double sign = 1.0;
 
+    public static double Rotate(Hardware hw, LinearOpMode lo, double targetDegrees, double power) {
+        double sign = 1.0;
+        if (targetDegrees < 0) {
+            sign = -1.0;
+            power *= sign;
+        }
         double target = Math.toRadians(targetDegrees);
         double hdelta = hw.pinpoint.getHeading() - target;
         while (hdelta < 0 && !lo.isStopRequested()) {
@@ -149,6 +216,7 @@ public class PathingMethods {
 
 
 }
+
 
 
 
