3af515bfef52e0c2cc736f1c9aba9ffa6aceffd0
Justin Holzer <justin@jtcholzer.net>
2025-01-20 05:47:32 -0700

    Update comments for pinpoint encoder offsets.  Force compile error so they get updated.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,4 +9,21 @@ public class Constants {
     static public final double wheelCircumference = 2.0*Math.PI*(wheelDiameterMm/2.0);
     static public final double revTicksPerRotation = 8192.0;
     static public final double encoderResolution = revTicksPerRotation/wheelCircumference;
+
+    // Force compile error so these are filled in.
+    // Offsets of encoders from center of rotation in inches.
+    // See page 3 of
+    // https://www.gobilda.com/content/user_manuals/3110-0002-0001%20User%20Guide.pdf
+    
+    // The arbitrary point in the diagram is where we want 0,0 field coordinate to be relative
+    // to the robot.  The measurements can be from the grabber to simplify pathing.
+
+    // For pinpoint y is strafe and x is forward.
+    // Offset along the x-axis from where you want 0,0 to be relative to the robot
+    // for the forward encoder.
+    public static final double xForwardEncoderOffsetInches = ;
+
+    // Offset along the y-axis from where you want 0,0 to be relative to the robot
+    // for the strafe encoder.
+    public static final double yStrafeEncoderOffsetInches =
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/PinpointDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/PinpointDrive.java
@@ -43,8 +43,9 @@ public class PinpointDrive extends MecanumDrive {
          */
         //These are tuned for 3110-0002-0001 Product Insight #1
         // RR localizer note: These units are inches, presets are converted from mm (which is why they are inexact)
-        public double xOffset = -3.3071;
-        public double yOffset = -6.6142;
+
+        public double xOffset = Constants.xForwardEncoderOffsetInches;
+        public double yOffset = Constants.yStrafeEncoderOffsetInches;
 
         /*
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
