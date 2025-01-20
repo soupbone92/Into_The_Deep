4c3f4e5269716a8d6d5cf5e9144b42810fd0d868
Justin Holzer <justin@jtcholzer.net>
2025-01-20 05:25:22 -0700

    Update readme and set constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -2,6 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 public class Constants {
     static public final double mmToInch = 1.0/25.4;
-    static public final double wheelCircumference = 109.955742875643;
-    static public final double encoderResolution = 8192 / wheelCircumference;
+
+    // encoderResolution is based on circumference of wheels and ticks per
+    // revolution of Rev encoders.
+    static public final double wheelDiameterMm = 35.0;
+    static public final double wheelCircumference = 2.0*Math.PI*(wheelDiameterMm/2.0);
+    static public final double revTicksPerRotation = 8192.0;
+    static public final double encoderResolution = revTicksPerRotation/wheelCircumference;
 }
