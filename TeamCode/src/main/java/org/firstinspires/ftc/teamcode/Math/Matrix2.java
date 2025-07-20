9d69f4f463fac721c0ddf601f71b7424939b09d9
Justin Holzer <justin@jtcholzer.net>
2025-07-20 15:59:01 -0700

    Add setIdentity to Matrix2.  Update comment.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Matrix2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Math/Matrix2.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-// Simple class to represent matrix for 2d vectors.
+// Simple class to represent matrix for transforming 2d vectors.
 public class Matrix2 {
     // Row major 2x3 matrix
     public double[] coeff = new double[6];
@@ -55,12 +55,19 @@ public class Matrix2 {
                 vec.y * coeff[3] + vec.y * coeff[4] + coeff[5]);
     }
 
+    public void setIdentity()
+    {
+        coeff[0] = 1;
+        coeff[1] = 0;
+        coeff[2] = 0;
+        coeff[3] = 1;
+        coeff[4] = 0;
+        coeff[5] = 0;
+    }
+
     @NonNull
     @Override
     public String toString() {
         return coeff[0] + "," + coeff[1] + "," + coeff[2] + ";" + coeff[3] + "," + coeff[4] + "," + coeff[5];
     }
-
-//    public final static Matrix2 rotccw90 = new Matrix2(90, AngleUnit.DEGREES);
-//    public final static Matrix2 rotcw90 = new Matrix2(-90, AngleUnit.DEGREES);
 }
